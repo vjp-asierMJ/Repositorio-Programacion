5176d5912a29620b2f809bc18d78b44b7e660d67
vjp-asierMJ <amartinj03@informatica.iesvalledeljerteplasencia.es>
2025-11-23 01:39:01 +0100

    Ejercicio25Tema5 Obligatorio

--- NetBeans/Tema 5/Ejercicio25Tema5/src/ejercicio25tema5/Ordenador.java
+++ NetBeans/Tema 5/Ejercicio25Tema5/src/ejercicio25tema5/Ordenador.java
@@ -9,5 +9,82 @@ package ejercicio25tema5;
  * @author alumno
  */
 public class Ordenador {
-    
+
+    private int memoriaRAM; //Variables
+    private int capacidadDisco;
+    private String modeloProcesador;
+    private String modeloGrafica;
+    private double precio;
+
+    // Constructor por defecto
+    public Ordenador() {
+        this.memoriaRAM = 0;
+        this.capacidadDisco = 0;
+        this.modeloProcesador = "";
+        this.modeloGrafica = "";
+        this.precio = 0.0;
+
+    }
+
+    // Constructor parametrizado
+    public Ordenador(int memoriaRAM, int capacidadDisco, String modeloProcesador,
+            String modeloGrafica, double precio) {
+        this.memoriaRAM = memoriaRAM;
+        this.capacidadDisco = capacidadDisco;
+        this.modeloProcesador = modeloProcesador;
+        this.modeloGrafica = modeloGrafica;
+        this.precio = precio;
+
+    }
+
+    // Getters y setters
+    public int getMemoriaRAM() {
+        return this.memoriaRAM;
+    }
+
+    public void setMemoriaRAM(int memoriaRAM) {
+        this.memoriaRAM = memoriaRAM;
+    }
+
+    public int getCapacidadDisco() {
+        return this.capacidadDisco;
+    }
+
+    public void setCapacidadDisco(int capacidadDisco) {
+        this.capacidadDisco = capacidadDisco;
+    }
+
+    public String getModeloProcesador() {
+        return modeloProcesador;
+    }
+
+    public void setModeloProcesador(String modeloProcesador) {
+        this.modeloProcesador = modeloProcesador;
+    }
+
+    public String getModeloGrafica() {
+        return this.modeloGrafica;
+    }
+
+    public void setModeloGrafica(String modeloGrafica) {
+        this.modeloGrafica = modeloGrafica;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Ordenador -> RAM: " + this.memoriaRAM
+                + "GB | Disco: " + this.capacidadDisco
+                + "GB | Procesador: " + this.modeloProcesador
+                + " | Gráfica: " + this.modeloGrafica
+                + " | Precio: " + this.precio + " euros";
+    }
+
 }
