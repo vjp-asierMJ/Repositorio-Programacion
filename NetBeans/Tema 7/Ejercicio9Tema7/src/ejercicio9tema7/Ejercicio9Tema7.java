88daded87547e834951b342b4b090c2eaf759663
vjp-asierMJ <amartinj03@informatica.iesvalledeljerteplasencia.es>
2026-01-12 18:51:27 +0100

    Ejercicio 8 y 9 Tema7

--- NetBeans/Tema 7/Ejercicio9Tema7/src/ejercicio9tema7/Ejercicio9Tema7.java
+++ NetBeans/Tema 7/Ejercicio9Tema7/src/ejercicio9tema7/Ejercicio9Tema7.java
@@ -24,31 +24,41 @@ public class Ejercicio9Tema7 {
         asignarArray(vector,num);
     }
     
-    public static int pedirNumero(){
+    public static int pedirNumero(){ //Metodo para pedir el numero
     int numero;
     
     Scanner entrada = new Scanner(System.in);
     
         System.out.println("Introduce un numero");
         numero = entrada.nextInt();
-        return numero;
+        return numero; //Devolvemos el numero
     
     }
     
-    public static int [] calcularCifras(int numero){
+    public static int [] calcularCifras(int numero){ //Metodo para saber las cifras
+    int contador = 0; //Creamos un contador
     
-    int [] array = new int [numero];
+    
+    while (numero> 0){ //Hacemos un bucle mientras que el numero sea mayor que 0
+       numero = numero/10; //Dividimos entre 10 por cada cifra
+       contador++; //Aumentamos el numero por cada cifra 
+       
+    }
+        System.out.println("El numero de cifras es : "+contador);
+    
+    
+    int [] array = new int [contador]; //Creamos un array y  le ponemos el contador
     return array;
     }
     
-    public static void asignarArray(int[] array, int numero){
+    public static void asignarArray(int[] array, int numero){ //Metodo para asignar los valores del array
         int i;
         
-        for(i=0;i<array.length;i++){
-            array[i] = numero%10;
-            numero = numero/10;
+        for(i=0;i<array.length;i++){ //Bucle para pasar por todas las posiciones del array
+            array[i] = numero%10; //Vamos cogiendo cada unidad
+            numero = numero/10; //Actualizamos el valor
             
-            System.out.println(array[i]);
+            System.out.println(array[i]); //Imprimimos el array al reves
         }
         
     }
