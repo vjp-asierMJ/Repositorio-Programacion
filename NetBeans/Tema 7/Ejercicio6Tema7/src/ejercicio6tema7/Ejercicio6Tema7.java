46acd2315f3acfdc00b0d14251e50a7bdc005612
vjp-asierMJ <amartinj03@informatica.iesvalledeljerteplasencia.es>
2026-01-23 20:13:27 +0100

    Ejercicio6Tema7

---
+++ NetBeans/Tema 7/Ejercicio6Tema7/src/ejercicio6tema7/Ejercicio6Tema7.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio6tema7;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio6Tema7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int [] array = new int [80]; //Array
+        
+        rellenarArray(array); //Metodos
+        mostrarArray(array);
+    }
+    
+    public static void rellenarArray(int [] array){ //Metodo para rellenar el array
+        int i,j;
+        i=0;
+        j=0;
+            
+        while(i<array.length) {
+            if (j == 2 || j == 3 || j == 5 || j == 7 ||
+           (j % 2 != 0 && j % 3 != 0 && j % 5 != 0 && j % 7 != 0)) {
+                
+                array[i] = j;
+                i++;
+            }
+            j++;
+            
+        }
+    
+    
+    }
+    
+    public static void mostrarArray(int [] array){
+    
+        int i,j;
+        j=0;
+        
+        for(i=0;i<array.length;i++){
+        
+            if(array[i]>0) {
+            System.out.println("["+j+"]"+" ->"+array[i]);
+            j++;
+        }
+        
+    }
+    
+}
+}
