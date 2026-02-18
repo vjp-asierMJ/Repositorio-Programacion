d39b1457154b97591d2cc20e28adb5962619b430
vjp-asierMJ <amartinj03@informatica.iesvalledeljerteplasencia.es>
2026-02-18 15:59:56 +0100

    Ejercicio9Tema9

--- NetBeans/Tema 9/Ejercicio9Tema9/src/ejercicio9tema9/Ejercicio9Tema9.java
+++ NetBeans/Tema 9/Ejercicio9Tema9/src/ejercicio9tema9/Ejercicio9Tema9.java
@@ -23,53 +23,61 @@ public class Ejercicio9Tema9 {
         String usuarioLower;
         String passwordLower;
         
-        int [] numeros = {1,2,3,4,5,6,7,8,9};
+        int [] numeros = {1,2,3,4,5,6,7,8,9}; //Array con los numeros
+      
+        
         
         int contador = 0;
+        int errores = 0;
         
         Scanner entrada = new Scanner(System.in);
         
         System.out.println("Introduce el usuario");
         
-        usuario = entrada.nextLine();
-        usuarioLower = usuario.toLowerCase();
+        usuario = entrada.nextLine(); //Pedimos el usuario
+        usuarioLower = usuario.toLowerCase(); //Lo ponemos en minusculas
         
-        System.out.println("Introduce la contraseña");
-        
-        password = entrada.nextLine();
-        passwordLower = password.toLowerCase();
-        
-        if(password.length() <6 ){
-        
-            System.out.println("La contraseña debe tener al menos 6 digitos");
-            System.out.println("Introduzca contraseña ");
-            
+                do { //Bucle
+
+            errores = 0; //Actualizamos los contadores
+            contador = 0;
+
+            System.out.println("Introduce la contraseña:");
             password = entrada.nextLine();
-        }
-        
-        
-        for(int i = 0; i<password.length();i++){
-        
-            for(int j = 0; j<password.length();j++){
-            
-                if(password.charAt(i) == numeros[j]){
-                
-                    contador++;
+            passwordLower = password.toLowerCase(); //Pasamos la contraseña a minusculas
+
+            // Longitud
+            if (password.length() < 6) { //Si es menor a 6
+                System.out.println("La contraseña debe tener al menos 6 caracteres.");
+                errores++; //Aumentamos el contador de errores
+            }
+
+            // Contar los dijitos
+            for (int i = 0; i < password.length(); i++) {
+
+                for (int j = 0; j < numeros.length; j++) { //Doble for para contar los dijitos
+
+                    if (Character.isDigit(password.charAt(i))) { //Por cada digito aumentamos el contador
+                        contador++;
+                    }
                 }
             }
-        }
+
+            if (contador < 2) { //Si tiene menos de dos digitos
+                System.out.println("La contraseña debe tener al menos 2 dígitos.");
+                errores++;
+            }
+
+            // Que no tenga el usuario en la contraseña
+            if (passwordLower.contains(usuarioLower)) {//Si contiene la contraseña aumentamos los errores
+                System.out.println("La contraseña no puede contener el nombre de usuario.");
+                errores++;
+            }
+
+        } 
+        while (errores != 0);//El bucle funciona mientras tenga algun error
         
-        if(contador < 2) {
-        
-            System.out.println("La contraseña debe tener almenos 2 digitos");
-        }
-        
-        if(passwordLower.contains(usuarioLower)){
-        
-            System.out.println("La contraseña no puede incluir el nombre de usuario");
-            System.out.println("Introduce contraseña");
-            password = entrada.nextLine();
-        }
-    }
+        System.out.println("Usuario creado correctamente");
     
+}
 }
